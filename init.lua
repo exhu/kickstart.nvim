5f6d065538f766d19d5675d533b26378e4ad9a19
Yury Benesh <yury.benesh@yandex.by>
2024-03-22 09:47:49 +0200

    copy file path shortcut

--- init.lua
+++ init.lua
@@ -855,3 +855,5 @@ vim.o.colorcolumn = '80'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.keymap.set('n', '<leader>p', "<cmd>let @+ = expand('%:p')<CR>", { desc = 'Copy file path to clipboard +' })
