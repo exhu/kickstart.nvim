40e6daa5787154d49fbc018c1968e4e82501517a
Yury Benesh <yury.benesh@yandex.by>
2024-03-27 19:21:12 +0200

    clang switch header/cpp shortcut

--- init.lua
+++ init.lua
@@ -497,6 +497,8 @@ require('lazy').setup({
           --  For example, in C this would take you to the header
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader>o', '<cmd>ClangdSwitchSourceHeader<CR>', 'Switch header/cpp')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
